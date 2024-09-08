6737f183661ca946903cb99726be95000100591b
Priyank9500 <priyankpatel6878@gmail.com>
2024-09-08 21:07:14 +0300

    Assignment

---
+++ hello.py
@@ -0,0 +1,8 @@
+age = input('What is your age? ')
+print('You are ' + age + ' years old')
+
+
+age_int = int(age)
+age_after = age_int + 10
+
+print('After 10 years, You are ' + str(age_after) + ' years old')
